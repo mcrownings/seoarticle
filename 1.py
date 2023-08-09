75122a01432d2214719397478dc0fe2b7c30431a
SwedenMax <m.crownings@gmail.com>
2023-08-09 17:19:20 +0200

    Added keywords

--- 1.py
+++ 1.py
@@ -57,7 +57,7 @@ def generate_content(prompt, previous_content="", language="English", keywords="
     # Initial message for GPT
     initial_message = {
         "role": "user",
-        "content": f"These keywords are important: {keywords}. Include one of these keyword per H2 heading and insert in the first paragraph, following the heading where the keyword has been used."
+        "content": f"These keywords are important: {keywords}. Make sure to include these keywords in headlines and paragraphs."
     }
 
     # Set the system message based on the chosen language
