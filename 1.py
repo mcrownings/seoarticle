bb2873490b1f88936531a06dd02783bee939a842
SwedenMax <m.crownings@gmail.com>
2023-08-17 23:56:53 +0200

    prompt

--- 1.py
+++ 1.py
@@ -36,7 +36,7 @@ If you want write more, in the end of the text state ---- (Press continue) ----.
     Skriv formaterade artiklar som kan kopieras och klistras in i ett CMS. 
     Om du vill ge din åsikt ska åsikten ska vara välinformerad och baserad på fakta. 
     Ibland kan du ge verkliga exempel.
-    Undvik summeringar, slutsatser och sammanfattningar.
+    Du ska INTE summera och UNDVIKA slutsatser och sammanfattningar.
     Skriv minst tre rubriker, jag förväntar mig en lång text.
 
     Jag kommer nu ge dig ett ämne, en målgrupp och sökord som du SKA inkludera i din text. 
